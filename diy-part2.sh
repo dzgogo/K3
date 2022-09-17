e3decbe2d9b27a4f20f8736bfc7a3dd25e9ef27e
saiwing <dzgogo@163.com>
2022-09-17 14:01:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 更改 golang 版本为 1.18.x
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
+# openwrt 21 更改 golang 版本为 1.18.x
+#rm -rf feeds/packages/lang/golang
+#svn export https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
