2113d8c8c37b9760c2ed0317934166a75c1edef6
saiwing <dzgogo@163.com>
2022-09-21 16:56:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,5 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # openwrt 21 更改 golang 版本为 1.18.x
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#svn export https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
